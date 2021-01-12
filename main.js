19468483b610245e79e5fd40ab2b9c9c802070f8
Giosuè Santoro <giosuesantoro@hotmail.it>
2021-01-12 16:23:07 +0100

    esercizio base completo

---
+++ main.js
@@ -0,0 +1,11 @@
+//Per mostrare menu
+
+$('.fas').click(function() {
+    $('.hamburger-menu').show();
+})
+
+//Per nascondere menu
+
+$('.close').click(function() {
+    $('.hamburger-menu').hide();
+})
